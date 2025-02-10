7176d132a9b3e9d7a0079d95c243072f2cd6e4ed
manvender singh <08manvender@gmail.com>
2025-02-10 13:35:05 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,73 @@
+const startBtn = document.querySelector(".startbtn")
+startBtn.addEventListener("click",()=>{
+
+function makeBubble() {
+
+    var clutter = "";
+    for (let i = 0; i <= 118; i++) {
+        clutter += `<div id class="bubble">${Math.floor(Math.random() * 10)}</div>`
+    }
+
+    document.querySelector("#pbtm").innerHTML = clutter
+}
+
+function bubbleEvent() {
+    document.querySelector("#pbtm").addEventListener("click", (e) => {
+        let value = e.target.textContent
+        let hitValue = document.querySelector("#hitBox")
+    //    console.log(hitValue.textContent)
+if(value===hitValue.textContent){
+    showScore()
+    makeBubble()
+    hitNum()
+}else{
+    makeBubble()
+}
+
+    })
+}
+function gameOver(){
+    document.querySelector("#pbtm").innerHTML = `<h1>GAME OVER<h1>`
+
+
+
+}
+
+var timer = 10;
+function runTimer() {
+    var timeInt = setInterval(() => {
+        if (timer > 0) {
+            timer--;
+            document.querySelector("#timerVal").textContent = timer
+        }
+        else {
+            clearInterval(timeInt)
+        }
+        if(timer===0){
+            gameOver()
+        }
+    }, 1000);
+}
+
+function hitNum() {
+    let num = Math.floor(Math.random() * 10);
+    document.querySelector("#hitBox").textContent = num
+}
+var score = 0
+function showScore() {
+    score += 10;
+    document.querySelector("#scoreVal").textContent = score
+}
+
+
+
+
+
+
+gameOver()
+bubbleEvent()
+hitNum()
+runTimer()
+makeBubble()
+
+})
